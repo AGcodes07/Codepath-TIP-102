30ea620404e2f285fad50da8c21170fd8fb2fc3b
AGcodes07 <gauliaad@msu.edu>
2026-06-21 13:12:19 +0530

    q04 updated

---
+++ Session1/ProblemSet1/q04.py
@@ -0,0 +1,32 @@
+"""
+Implement a function get_item() that accepts a 0-indexed list items and a non-negative integer x and returns the element at index x in items. If x is not a valid index of items, return None.
+
+Example Usage
+
+items = ["piglet", "pooh", "roo", "rabbit"]
+x = 2
+get_item(items, x)
+
+items = ["piglet", "pooh", "roo", "rabbit"]
+x = 5
+get_item(items, x)
+Example Output:
+
+"roo"
+None
+
+"""
+def get_item(items, x):
+    if x < len(items):
+        return items[x]
+    else:
+        return None
+    
+
+items = ["piglet", "pooh", "roo", "rabbit"]
+x = 2
+print(get_item(items, x))   # should print "roo"
+
+items = ["piglet", "pooh", "roo", "rabbit"]
+x = 5
+print(get_item(items, x))   # should print None
