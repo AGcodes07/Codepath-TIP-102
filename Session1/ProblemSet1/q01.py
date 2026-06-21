55fbefdb8a4ba2b5a47aa3df8343383daeed0ac5
AGcodes07 <gauliaad@msu.edu>
2026-06-21 11:21:44 +0530

    Solve Sessio1 ProblemSet 1 Q1: welcome function

---
+++ Session1/ProblemSet1/q01.py
@@ -0,0 +1,21 @@
+"""
+Session 1 - Problem Set 1 - Question 1
+Hundred Acre Wood
+
+Problem:
+Write a function welcome() that prints the string
+"Welcome to The Hundred Acre Wood!"
+
+Example Usage:
+welcome()
+
+Example Output:
+Welcome to The Hundred Acre Wood!
+"""
+
+def welcome():
+    print("Welcome to The Hundred Acre Wood!")
+
+
+if __name__ == "__main__":
+    welcome()
