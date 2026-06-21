d418871b1b7fda858c7627c101194066d6542f13
AGcodes07 <gauliaad@msu.edu>
2026-06-21 14:06:09 +0530

    q08

---
+++ Session1/ProblemSet1/q08.py
@@ -0,0 +1,37 @@
+"""
+Write a function print_todo_list() that accepts a list of strings named tasks. The function should then number and print each task on a new line using the format:
+
+Pooh's To Dos:
+1. Task 1
+2. Task 2
+...
+
+Example Usage
+
+tasks = ["Count all the bees in the hive", "Chase all the clouds from the sky", "Think", "Stoutness Exercises"]
+print_todo_list(tasks)
+
+tasks = []
+print_todo_list(tasks)
+Example Output:
+
+Pooh's To Dos:
+1. Count all the bees in the hive
+2. Chase all the clouds from the sky
+3. Think
+4. Stoutness Exercises
+
+Pooh's To Dos:
+
+"""
+
+def print_todo_list(tasks):
+	print("Pooh's To Dos:")
+	for i, task in enumerate(tasks):
+		print(f"{i+1}. {task}")
+	
+tasks = ["Count all the bees in the hive", "Chase all the clouds from the sky", "Think", "Stoutness Exercises"]
+print_todo_list(tasks)
+
+tasks = []
+print_todo_list(tasks)
