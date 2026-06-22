5c972748a5d704a01ab48d2d22e63f6e33530e68
AGcodes07 <gauliaad@msu.edu>
2026-06-22 10:33:04 +0530

    Q04 problem set 2

---
+++ Session1/ProblemSet2/q04.py
@@ -0,0 +1,27 @@
+"""Implement a function get_last() that accepts a list of items items and returns the last item in the list. If the list is empty, return None.
+
+
+Example Usage
+
+items = ["spider man", "batman", "superman", "iron man", "wonder woman", "black adam"]
+get_last(items)
+
+items = []
+get_last(items)
+Example Output:
+
+"black adam"
+None
+"""
+
+def get_last(items):
+	if len(items) == 0:
+		return None
+	else: 
+		return items[len(items) - 1]
+
+items = ["spider man", "batman", "superman", "iron man", "wonder woman", "black adam"]
+print(get_last(items))
+
+items = []
+print(get_last(items))git add . && git commit -m"Q04 problem set 2" && git push
