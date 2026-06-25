f152bf2f854279c97bb0097ca72cbaf65505a8ae
AGcodes07 <gauliaad@msu.edu>
2026-06-25 12:31:01 +0530

    session 2 problem set 1 q02

---
+++ Session2/ProblemSet1/q02.py
@@ -0,0 +1,35 @@
+"""In the extended universe of fictional bears, Goldilocks finds an enticing list of numbers in the Three Bears' house. She doesn't want to take a number that's too high or too low - she wants a number that's juuust right. Write a function goldilocks_approved() that takes in the list of distinct positive integers nums and returns any number from the list that is neither the minimum nor the maximum value in the array, or -1 if there is no such number.
+
+Return the selected integer.
+
+Example Usage
+
+nums = [3, 2, 1, 4]
+goldilocks_approved(nums)
+
+nums = [1, 2]
+goldilocks_approved(nums)
+
+nums = [2, 1, 3]
+goldilocks_approved(nums)
+Example Output:
+
+2
+-1
+2
+"""
+
+def goldilocks_approved(nums):
+    for num in nums:
+        if num != min(nums) and num != max(nums):
+            return num
+        return -1 
+   
+nums = [3, 2, 1, 4]
+print(goldilocks_approved(nums))
+
+nums = [1, 2]
+print(goldilocks_approved(nums))
+
+nums = [2, 1, 3]
+print(goldilocks_approved(nums))
