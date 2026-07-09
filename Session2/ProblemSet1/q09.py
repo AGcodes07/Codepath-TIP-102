b07da855a91f413dc01cf196443b06ef10604ce8
AGcodes07 <gauliaad@msu.edu>
2026-07-09 14:16:38 +0530

    q09 updated

--- Session2/ProblemSet1/q09.py
+++ Session2/ProblemSet1/q09.py
@@ -1,8 +1,6 @@
 """
 Write a function common_elements() that takes in two lists lst1 and lst2 and returns a list of the elements that are common to both lists.
 
-def common_elements(lst1, lst2):
-    pass
 Example 1:
 Input: lst1 = ["super strength", "super speed", "x-ray vision"], lst2 = ["super speed", "time travel", "dimensional travel"]
 Output: ["super speed"]
@@ -24,3 +22,18 @@ Example Output:
 ["super speed"]
 []
 """
+
+def common_elements(lst1, lst2):
+    new_list = []
+    for item in lst1:
+        if item in lst2:
+            new_list.append(item)
+    return new_list 
+
+lst1 = ["super strength", "super speed", "x-ray vision"]
+lst2 = ["super speed", "time travel", "dimensional travel"]
+print(common_elements(lst1, lst2))
+
+lst1 = ["super strength", "super speed", "x-ray vision"]
+lst2 = ["martial arts", "stealth", "master detective"]
+print(common_elements(lst1, lst2))
