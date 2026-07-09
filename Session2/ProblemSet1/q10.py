bdbdbe2ad8aae4c304c66e0ca3a0dcbf1c5c1cbd
AGcodes07 <gauliaad@msu.edu>
2026-07-09 18:14:41 +0530

    q10 updated

--- Session2/ProblemSet1/q10.py
+++ Session2/ProblemSet1/q10.py
@@ -10,8 +10,6 @@ Write a function expose_superman() that accepts a 2D array trust where trust[i] 
 
 Return the label of Superman if he is hiding amongst the population and can be identified, or return -1 otherwise.
 
-def expose_superman(trust, n):
-    pass
 Example Usage
 
 n = 2
@@ -31,3 +29,27 @@ Example Output:
 3
 -1
 """
+def expose_superman(trust, n):
+    trusts_count = [0] * (n + 1)
+    trusted_by_count = [0] * (n + 1)
+    
+    for a, b in trust:
+        trusts_count[a] += 1
+        trusted_by_count[b] += 1
+    
+    for person in range(1, n + 1):
+        if trusts_count[person] == 0 and trusted_by_count[person] == n - 1:
+            return person
+    return -1
+
+n = 2
+trust = [[1, 2]]
+print(expose_superman(trust, n))
+
+n = 3
+trust = [[1, 3], [2, 3]]
+print(expose_superman(trust, n))
+
+n = 3
+trust = [[1, 3], [2, 3], [3, 1]]
+print(expose_superman(trust, n))
